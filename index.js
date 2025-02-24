c6c2e5c2c2585fb2480db9ac9d45b46944f7c8f4
Katerina Matejovska <katerinamatejovska@email.cz>
2025-02-24 20:36:23 +0100

    lekce 5

--- index.js
+++ index.js
@@ -15,3 +15,24 @@ const product3 = {
   name: 'Anatoplavky',
   description: 'Aby muži na rozpálených plážích kromě vašeho těla obdivovali také vaše nitro.',
 };
+
+
+const firstProductEl = document.querySelector('#product1');
+firstProductEl.classList.add('border-primary');
+
+const secondButtonEl = document.querySelector('#product2 button');
+secondButtonEl.classList.remove('btn-primary');
+
+const cardTitleEl = document.querySelector('#product3 h2');
+cardTitleEl.classList.toggle('text-center');
+
+
+const product1HTML = `
+  <img class="card-img-top" src="images/destniklobouk.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h2 class="card-title">Deštníklobouk</h2>
+    <p class="card-text">Vynikající pokud si často zapomínáte deštník a nechcete být nikdy překvapeni nečekanou přeháňkou.</p>
+  </div>
+  <button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>
+`;
+
